324cd1f6a5258f6326b55d8844d55679fdcba0de
sramkumar2k2-cmd <sramkumar2k2@gmail.com>
2025-12-06 18:18:14 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // Allow cross-origin requests from local network devices during development
-  allowedDevOrigins: [
-    "192.168.137.1",
-    "192.168.1.1",
-    "172.19.192.1",
-    "localhost",
-  ],
 };
 
 export default nextConfig;
